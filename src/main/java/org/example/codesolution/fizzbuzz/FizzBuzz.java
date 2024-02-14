bf1fbf88b8243d20848d689e306e7fd6ae3b49a3
bayazidsustami <bayazidsustamy@gmail.com>
2024-02-14 20:38:32 +0800

    feat : improve code readability

--- src/main/java/org/example/codesolution/fizzbuzz/FizzBuzz.java
+++ src/main/java/org/example/codesolution/fizzbuzz/FizzBuzz.java
@@ -3,14 +3,20 @@ package org.example.codesolution.fizzbuzz;
 public class FizzBuzz {
 
     public static String fizzBuzz(int n) {
-        if (n % 3 == 0 && n % 5 == 0) {
-            return "FizzBuzz";
-        } else if (n % 3 == 0) {
-            return "Fizz";
-        } else if (n % 5 == 0) {
-            return "Buzz";
-        } else {
-            return "";
+        if (n <= 0) {
+            throw new IllegalArgumentException("Input must be a positive integer");
         }
+
+        StringBuilder result = new StringBuilder();
+
+        if (n % 3 == 0) {
+            result.append("Fizz");
+        }
+
+        if (n % 5 == 0) {
+            result.append("Buzz");
+        }
+
+        return !result.isEmpty() ? result.toString() : String.valueOf(n);
     }
 }
